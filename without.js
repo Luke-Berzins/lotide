5c19a07a3ca26aa484c9cd7bbb0252d562d5b701
luke-berzins <lukeberzins16@gmail.com>
2020-11-24 18:21:26 +0000

    without.js added

---
+++ without.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(arr1, arr2) {
+  let counter = 0;
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`👎👎👎 not equal br0: ${arr1} !== ${arr2} 👎👎👎`);
+      break;
+    } else {
+      counter += 1;
+    }
+  }
+  if (counter === arr1.length) {
+    console.log(`🍔 🍔 🍔 delicious code br0 (equal arrays): ${arr1} === ${arr2}🍔 🍔 🍔 `);
+  }
+};
+
+
+const without = function(source, itemsToRemove) {
+  withoutArray = [];
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        break;
+      }
+      if (j === itemsToRemove.length - 1) {
+        withoutArray.push(source[i])
+      }
+    }
+  }
+  return withoutArray;
+}
+console.log(without([1, 2, 3], [1]))
+console.log(without(["1", "2", "3"], [1, 2, "3"]))
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]))
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]))
