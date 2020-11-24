df075bacd9ca967a283352efcf5b8b72628f79dc
luke-berzins <lukeberzins16@gmail.com>
2020-11-24 20:23:56 +0000

    using the old functions instead of making a new one

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚 🚗 💨 WE MOVIN NOW (correct): ${actual} === ${expected} 🚗 💨💚 `);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 STOP THAT CAR: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+    if (eqArrays(arr1, arr2)) {
+      console.log(`👎👎👎 not equal br0: ${arr1} !== ${arr2} 👎👎👎`);
+    } else 
+      console.log(`🍔 🍔 🍔 delicious code br0 (equal arrays): ${arr1} === ${arr2}🍔 🍔 🍔 `)
+};
+
+
