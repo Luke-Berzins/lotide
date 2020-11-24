f0de9d4e70d390b502db8e1473afa53d17eafd6d
luke-berzins <lukeberzins16@gmail.com>
2020-11-24 18:23:01 +0000

    linted version

--- without.js
+++ without.js
@@ -12,24 +12,22 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`🍔 🍔 🍔 delicious code br0 (equal arrays): ${arr1} === ${arr2}🍔 🍔 🍔 `);
   }
 };
-
-
 const without = function(source, itemsToRemove) {
-  withoutArray = [];
+  let withoutArray = [];
   for (let i = 0; i < source.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
       if (source[i] === itemsToRemove[j]) {
         break;
       }
       if (j === itemsToRemove.length - 1) {
-        withoutArray.push(source[i])
+        withoutArray.push(source[i]);
       }
     }
   }
   return withoutArray;
-}
-console.log(without([1, 2, 3], [1]))
-console.log(without(["1", "2", "3"], [1, 2, "3"]))
+};
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
 const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"]))
-console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]))
+console.log(without(words, ["lighthouse"]));
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
