accdf86b2ad72ebb502b2d2473f6d5a443f864d9
luke-berzins <lukeberzins16@gmail.com>
2020-11-25 18:47:03 +0000

    counting letters!

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚 🚗 💨 WE MOVIN NOW (correct): ${actual} === ${expected} 🚗 💨💚 `);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 STOP THAT CAR: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+const countLetters = function(stringOfLetters) {
+  stringOfLetters = stringOfLetters.replace(/ /g, "");
+  const results = {};
+  for (const letter of stringOfLetters) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
