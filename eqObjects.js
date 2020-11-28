93a93cd77dee8f29c088ae008320f07a5326f268
luke-berzins <lukeberzins16@gmail.com>
2020-11-28 23:40:37 +0000

    using recursion initial!

--- eqObjects.js
+++ eqObjects.js
@@ -14,32 +14,28 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-  for (let secondKeys in object2) {
-    if (object2[secondKeys] !== object1[secondKeys]) {
-      if (Array.isArray(object2[secondKeys])) {
-        if (eqArrays(object2[secondKeys], object1[secondKeys]) !== true) {
-          return false;
-        }
-      } else {
-        return false;
-      }
+  /*let firstK = Object.keys(object1);
+  firstK.forEach(a => {
+    if (typeof(object1[a]) === "object") {
+      return eqObjects(object1[a], object2[a]);
+    }
+    console.log(object1[a], "?", object2[a])
+    if (object1[a] !== object2[a]) {
+      flag += 1;
+    }
+  }) */ 
+  // THIS WONT WORK WITH FOREACH :( AN HOUR GONE :())
+  for (firstK in object1) {
+    if (typeof(object1[firstK]) === "object") {
+      return eqObjects(object1[firstK], object2[firstK]);
+    }
+    if (object1[firstK] !== object2[firstK]) {
+      return false
     }
   }
-  return true;
+  return true
 };
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(assertEqual(eqObjects(ab, ba), true));
-const abc = { a: "1", b: "2", c: "3" };
-console.log(assertEqual(eqObjects(ab, abc), false));
-const abb = { a: "1", b: "2", c: "4", d: 5 };
-const bba = { b: "2", a: "1", c: "4", d: 5 };
-console.log(assertEqual(eqObjects(abb, bba), true));
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(assertEqual(eqObjects(cd, dc), true));
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertEqual(eqObjects(cd, cd2), false));
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
+assertEqual(eqObjects({ a: { z: { t : 10, v : { s : 1, d : {u :6, h: 1}}} }, b: 2 }, { a: { z: { t : 10, v : { s : 1, d : {u :6, h: 1}}} }, b: 2 }), true)
+
