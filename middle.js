dd3142b8c917afcd1d04f8602d240a08ccc33c53
luke-berzins <lukeberzins16@gmail.com>
2020-11-30 18:51:23 +0000

    middle refactored and test added

--- middle.js
+++ middle.js
@@ -1,17 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i ++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🍔 🍔 🍔 delicious code br0 (equal arrays): ${arr1} === ${arr2}🍔 🍔 🍔 `);
-  } else console.log(`👎👎👎 not equal br0: ${arr1} !== ${arr2} 👎👎👎`);
-};
-
 const middle = function(middleArr) {
   let collecter = [];
   if (middleArr.length <= 2) {
@@ -26,9 +12,5 @@ const middle = function(middleArr) {
   }
   return collecter;
 };
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+
+module.exports = middle;
